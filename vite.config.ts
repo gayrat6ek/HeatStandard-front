838a003ceb69d0a22aef87485b5ce1e0c0748fd8
Gayratbek <gayratbekakhmedov@gmail.com>
2026-01-14 22:22:59 +0500

    ad

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8002',
+        target: 'http://109.199.100.137:8002',
         changeOrigin: true,
       },
     },
